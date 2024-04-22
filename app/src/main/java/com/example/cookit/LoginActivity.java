863345f60ae947cf258f7a990a5e68799c7320ad
00sergio <sergigonza72@gmail.com>
2024-04-22 23:13:28 +0200

    2º entrega (no final)

---
+++ app/src/main/java/com/example/cookit/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.example.cookit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin, buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize views
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonRegister = findViewById(R.id.registerButton);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
+        });
+
+        Button buttonRegistro = findViewById(R.id.registerButton);
+
+
+        buttonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ir a la actividad de registro
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+        private void iniciarSesion() {
+            // Obtener referencias a los campos de usuario y contraseña
+            EditText editTextUsuario = findViewById(R.id.editTextUsername);
+            EditText editTextContrasena = findViewById(R.id.editTextPassword);
+
+            // Obtener los valores de usuario y contraseña
+            String usuario = editTextUsuario.getText().toString();
+            String contrasena = editTextContrasena.getText().toString();
+
+            // Enviar solicitud al servidor para verificar la autenticación
+            String url = "http://35.223.65.65:81/login.php"; // URL del script para verificar la autenticación
+            String parametros = "usuario=" + Uri.encode(usuario) + "&contrasena=" + Uri.encode(contrasena);
+
+            // Ejecutar la tarea asíncrona para enviar la solicitud
+            new VerificarLoginTask().execute(url, parametros);
+        }
+
+        private class VerificarLoginTask extends AsyncTask<String, Void, Boolean> {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                String urlString = params[0];
+                String parametros = params[1];
+                boolean autenticado = false;
+
+                try {
+                    // Crear la conexión HTTP
+                    URL url = new URL(urlString);
+                    HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+                    conexion.setRequestMethod("POST");
+                    conexion.setDoOutput(true);
+
+                    // Escribir los parámetros en la solicitud
+                    OutputStream outputStream = conexion.getOutputStream();
+                    outputStream.write(parametros.getBytes(StandardCharsets.UTF_8));
+                    outputStream.flush();
+                    outputStream.close();
+
+                    // Leer la respuesta del servidor
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains("Autenticado")) {
+                            autenticado = true;
+                        }
+                    }
+                    reader.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return autenticado;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean autenticado) {
+                if (autenticado) {
+                    // Si el usuario está autenticado, iniciar la actividad del menú
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Si no está autenticado, mostrar un mensaje de error
+                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+    }
+}
---
+++ app/src/main/java/com/example/cookit/RegisterActivity.java
@@ -0,0 +1,113 @@
+package com.example.cookit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Botón de registro
+        Button buttonRegistro = findViewById(R.id.registerButton);
+
+        // Configurar el click listener del botón
+        buttonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarUsuario();
+            }
+        });
+    }
+
+    private void registrarUsuario() {
+        // Obtener referencias a los campos de usuario y contraseña
+        EditText editTextUsuario = findViewById(R.id.registerEditTextUsername);
+        EditText editTextContrasena = findViewById(R.id.registerEditTextPassword);
+
+        // Obtener los valores de usuario y contraseña
+        String usuario = editTextUsuario.getText().toString();
+        String contrasena = editTextContrasena.getText().toString();
+
+        // Enviar solicitud al servidor para registrar el usuario
+        String url = "http://35.223.65.65:81/registro.php"; // URL del script para procesar el registro
+        String parametros = "usuario=" + Uri.encode(usuario) + "&contrasena=" + Uri.encode(contrasena);
+
+        // Ejecutar la tarea asíncrona para enviar la solicitud
+        new RegistrarUsuarioTask().execute(url, parametros);
+    }
+
+    // Clase para registrar usuario en segundo plano
+    private class RegistrarUsuarioTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            // Obtener la URL y los parámetros de los argumentos
+            String urlString = params[0];
+            String parametros = params[1];
+            String respuesta = "";
+
+            try {
+                // Crear la conexión HTTP
+                URL url = new URL(urlString);
+                HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+
+                // Configurar la conexión
+                conexion.setRequestMethod("POST");
+                conexion.setDoOutput(true);
+
+                // Escribir los parámetros en la solicitud
+                OutputStream outputStream = conexion.getOutputStream();
+                outputStream.write(parametros.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+                outputStream.close();
+
+                // Leer la respuesta del servidor
+                int responseCode = conexion.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // La solicitud fue exitosa
+                    // Leer la respuesta del servidor
+                    respuesta = "Usuario registrado exitosamente";
+                } else {
+                    // La solicitud falló
+                    respuesta = "Error al registrar el usuario: " + conexion.getResponseMessage();
+                }
+
+                // Cerrar la conexión
+                conexion.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+                respuesta = "Error al registrar el usuario: " + e.getMessage();
+            }
+
+            return respuesta;
+        }
+
+        @Override
+        protected void onPostExecute(String respuesta) {
+            // Mostrar el mensaje de confirmación
+            Toast.makeText(RegisterActivity.this, respuesta, Toast.LENGTH_SHORT).show();
+
+            // Si el registro fue exitoso, volver a la página de inicio de sesión
+            if (respuesta.equals("Usuario registrado exitosamente")) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finalizar la actividad actual para que el usuario no pueda volver atrás
+            }
+        }
+    }
+}
